c900d842ea0f8726b3272475f521389fa8435a76
marcossch <schapiramarcos@gmail.com>
2017-04-25 11:02:09 -0300

    subo el container 3D

---
+++ Container3d/Container.js
@@ -0,0 +1,44 @@
+class Container3D {
+    constructor() {
+        this.matrix = null;
+        this.children = [];
+        this.initMatrix();
+    }
+
+    initMatrix() {
+        //inicializa la matriz como la identidad
+        this.matrix = mat4.create();
+        mat4.identity(this.matrix);
+    }
+
+    translate(x, y, z) {
+        mat4.translate(this.matrix, this.matrix, vec3.fromValues(x, y, z));
+    }
+
+    scale(x, y, z) {
+        mat4.scale(this.matrix, this.matrix, vec3.fromValues(x, y, z));
+    }
+
+    rotate(angulo, x, y, z) {
+        mat4.rotate(this.matrix, this.matrix, angulo, vec3.fromValues(x, y, z));
+    }
+
+    add(child) {
+        //recibe un hijo de tipo Objeto3d para agregar a su jerarquia
+    }
+
+    remove(child) {
+        //recibe un hijo
+    }
+
+    resetMatrix() {
+        //resetea la matriz como la identidad
+        initMatrix();
+    }
+
+    applyMatrix(matrix) {
+        //recibe una matriz la cual multiplica por la suya propia
+        mat4.multiply(this.matrix, this.matrix, matrix);
+    }
+
+}
