28f5bc69f3de0d5962049ab2a504b9ec0dee92d9
marcossch <schapiramarcos@gmail.com>
2017-04-30 16:55:06 -0300

    cambio calc buffer

--- Container3d/BufferCalculator.js
+++ Container3d/BufferCalculator.js
@@ -17,18 +17,61 @@ class BufferCalculator{
     this.colorBuffer = [];
     this.normalBuffer = [];
     this.indexBuffer = [];
+    this.latitudeBands = 64;
+    this.longitudeBands = 64;
     }
 
     calculateBuffer(){
 
-      if( this.figura == CILINDRO){
-        this.calcBuffersCil();
-      }
+        var latNumber;
+        var longNumber;
 
-      if( this.figura == CUBO ){
-        this.calcBuffersCubo();
-      }
-      this.calcIndexBuffer();
+        for (latNumber=0; latNumber <= this.latitudeBands; latNumber++) {
+            var theta = latNumber * Math.PI / this.latitudeBands;
+            var sinTheta = Math.sin(theta);
+            var cosTheta = Math.cos(theta);
+
+            for (longNumber=0; longNumber <= this.longitudeBands; longNumber++) {
+                var phi = longNumber * 2 * Math.PI / this.longitudeBands;
+                var sinPhi = Math.sin(phi);
+                var cosPhi = Math.cos(phi);
+
+                var x = cosPhi * sinTheta;
+                var y = cosTheta;
+                var z = sinPhi * sinTheta;
+                var u = 1.0 - (longNumber / this.longitudeBands);
+                var v = 1.0 - (latNumber / this.latitudeBands);
+
+                this.normalBuffer.push(x);
+                this.normalBuffer.push(y);
+                this.normalBuffer.push(z);
+
+                // Mejorar o modificar el algoritmo que inicializa
+                // el color de cada vertice
+                this.colorBuffer.push(x/2.0)
+                this.colorBuffer.push(y/2.0)
+                this.colorBuffer.push(z/2.0)
+
+                this.posBuffer.push(x);
+                this.posBuffer.push(y);
+                this.posBuffer.push(z);
+            }
+        }
+
+
+        for (latNumber=0; latNumber < this.latitudeBands; latNumber++) {
+            for (longNumber=0; longNumber < this.longitudeBands; longNumber++) {
+                var first = (latNumber * (this.longitudeBands + 1)) + longNumber;
+                var second = first + this.longitudeBands + 1;
+                this.indexBuffer.push(first);
+                this.indexBuffer.push(second);
+                this.indexBuffer.push(first + 1);
+
+                this.indexBuffer.push(second);
+                this.indexBuffer.push(second + 1);
+                this.indexBuffer.push(first + 1);
+            }
+        }
     }
 
     calcBuffersCil(radio){
@@ -47,6 +90,10 @@ class BufferCalculator{
                 this.posBuffer.push(i-( (this.rows-1) + (sentido*2))/2.0);
                 //La variable Z se define como R*Sen(theta)
                 this.posBuffer.push(0);
+
+                this.colorBuffer.push(Math.cos(theta*j) / 2.0);
+                this.colorBuffer.push(i-( (this.rows-1) + (sentido*2))/2.0);
+                this.colorBuffer.push(0);
               }
               else {
                 // Para cada vertice definimos su posicion
@@ -57,11 +104,13 @@ class BufferCalculator{
                 this.posBuffer.push(i-(this.rows-1)/2.0);
                 //La variable Z se define como R*Sen(theta)
                 this.posBuffer.push(radioAux*Math.sin(theta*j));
+
+                this.colorBuffer.push(radioAux*Math.cos(theta*j) / 2.0);
+                this.colorBuffer.push(i-(this.rows-1)/2.0);
+                this.colorBuffer.push(radioAux*Math.sin(theta*j) / 2.0);
               }
               // Para cada vertice definimos su color
-              this.colorBuffer.push(1.0/this.rows * i);
-              this.colorBuffer.push(0.2);
-              this.colorBuffer.push(1.0/this.colms * j);
+
           }
       }
     }
