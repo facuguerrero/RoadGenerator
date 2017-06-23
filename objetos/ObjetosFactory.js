cba109623fc4c9cf393655f4bbd388a558e29a08
facundo <facundoiguerrero@gmail.com>
2017-06-23 11:18:41 -0300

    Esfera acomodada

--- objetos/ObjetosFactory.js
+++ objetos/ObjetosFactory.js
@@ -414,7 +414,7 @@ class ObjetosFactory {
         escene.add(linea);
         if(bool) {
             var sky = this.createSky();
-            sky.translate(x/2, -50.0 ,x/2);
+            sky.translate(x/2, -75.0 ,x/2);
             escene.add(sky);
         }
         escene.build();
